7a9e05122b48107c018faa203f17780954f31b7c
Michael Werbowsky <mwerbo2@gmail.com>
2019-10-15 22:12:20 -0500

    User get model added

--- db/config.js
+++ db/config.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var sql = mysql.createConnection({
     host: process.env.db_host,
     user: process.env.db_username,
     password: process.env.db_password,
     database: process.env.db_name
 });
  
-module.exports = connection;
+module.exports = sql;
