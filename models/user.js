c7c1f1a1d174ddca57021715a531997092fc43e5
Michael Werbowsky <mwerbo2@gmail.com>
2019-10-18 22:07:25 -0500

    Add in user create method

--- models/user.js
+++ models/user.js
@@ -15,4 +15,17 @@ User.getAllUsers = (result) => {
     })
 };
 
+User.createUser = (req, result) => {
+    sql.query('INSERT INTO users (first_name, last_name, email, password_digest, validated, avatar) VALUES ($1, $2, $3, $4, $5, $6)', [req.body.first_name, req.body.lastname, req.body.email, req.body.password_digest, req.body.validated, req.body.avatar], (err, res) => {
+        if (err) {
+            result(err, null)
+        } else {
+            return result(null, res)
+        }
+    })
+};
+
+
+
+
 module.exports = User;
