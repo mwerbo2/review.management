d4347f41c8a19e9b6ea64fd6b05bc4d466671d7b
Michael Werbowsky <mwerbo2@gmail.com>
2019-10-19 22:05:43 -0500

    Add in user get route

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-
 const app = express();
+
 
 app.listen(PORT, () => {
 console.log(`Server running on port: ${PORT}`)
