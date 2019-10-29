dab2898e0f3f91e8273e4115f05f74f24e768abb
Christopher Rankin <devon.rankin@gmail.com>
2019-10-28 21:53:51 -0500

    Add in user create controller

--- controllers/users.js
+++ controllers/users.js
@@ -9,4 +9,14 @@ const getAllUsers = async (req, res) => {
     }
 }
 
+const createUser = async (req, res) => {
+    try {
+        const {first_name, last_name, email, password_digest, avatar } = req.body
+        const user = await User.createUser(first_name, last_name, email, password_digest, avatar)
+        return res.status(201).send(user)
+    } catch (error) {
+        return res.status(400).send(error)
+    }
+ 
+}
 export { getAllUsers }; 
