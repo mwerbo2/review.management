8f13ee7c957dac25d8d9b38b9ef4b7173e7d51a8
Michael Werbowsky <mwerbo2@gmail.com>
2019-12-03 23:41:13 -0600

    Update building route

--- routes/buildings.js
+++ routes/buildings.js
@@ -1,5 +1,6 @@
-// const { findBuildingById, createBuilding } = require('../controllers/buildings')
+// const { findBuildingById, createBuilding, updateBuilding } = require('../controllers/buildings')
 module.exports = (app) => {
     app.get('/api/buildings/:id', findBuildingById),
-    app.post('/api/buildings/', createBuilding)
+    app.post('/api/buildings/', createBuilding),
+    app.put('/api/buildings/:id', updateBuilding)
 }
