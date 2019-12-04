f5c5a3cbea8bde7d94bcf767c15ce0e89667895a
Michael Werbowsky <mwerbo2@gmail.com>
2019-12-03 23:47:18 -0600

    Create building controller

--- controllers/buildings.js
+++ controllers/buildings.js
@@ -14,6 +14,18 @@ const findBuildingById = (req, res) => {
     };
 };
 
+const createBuilding = (req, res) => {
+    try {
+        Building.createBuilding(req.body, (building, err) => {
+            if (err) res.status(400).send(err)
+            res.status(200).send(building);
+        })
+    } catch (error) {
+        res.status(400).send(error)
+    };
+}
+
 module.exports = {
-    findBuildingById
+    findBuildingById,
+    createBuilding
 }
