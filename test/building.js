e9e3ed285cf870ef17a13561ed793cab82851738
Michael Werbowsky <mwerbo2@gmail.com>
2019-12-24 23:53:43 -0600

    Put and post building test

--- test/building.js
+++ test/building.js
@@ -18,3 +18,33 @@ describe('Building', () => {
         })
     })
 })
+
+describe('Building', () => {
+    it('Should insert a building', (done) => {
+        chai.request(app)
+        .post('http://localhost:5000/api/buildings').send({
+            "name": "test buildign",
+            "street_number": "23",
+            "street_name": "test"
+        })
+        .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        done()
+        })
+    })
+})
+
+describe('Building', () => {
+    it('Should return a building by id', (done) => {
+        chai.request(app)
+        .put('http://localhost:5000/api/buildings/2').send({
+            "name":"new test"
+        })
+        .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        done()
+        })
+    })
+})
