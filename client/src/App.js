f169c2191a73bfedb4184e52a8db3846a86d6745
Michael Werbowsky <mwerbo2@gmail.com>
2019-12-28 21:31:13 -0600

    Router set up with Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-        <p>Property Review</p>
-    </div>
+    <Router>
+      <Fragment>
+        <Route exact path='/' component={Home} />
+      </Fragment>
+    </Router>
   );
 }
 
