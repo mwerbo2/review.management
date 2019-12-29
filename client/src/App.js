c19dd380014e9cf48a4a2f11d55b4c110e2668b9
Michael Werbowsky <mwerbo2@gmail.com>
2019-12-28 23:44:56 -0600

    Set up nav with home, post and login links

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 
