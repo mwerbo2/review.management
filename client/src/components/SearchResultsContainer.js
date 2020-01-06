f5bd9d5bceecd4fd458b45cb202e0ef32ae8187d
Michael Werbowsky <mwerbo2@gmail.com>
2020-01-05 22:03:48 -0600

    Result list item with description and ratings

--- client/src/components/SearchResultsContainer.js
+++ client/src/components/SearchResultsContainer.js
@@ -4,13 +4,14 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import MapView from './MapView';
+import ResultsList from './ResultsList';
 
 export default function SearchResultsContainer() {
     return (
         <Container fluid>
             <Row>
             <Col><MapView /></Col>
-            <Col>List</Col>
+            <Col><ResultsList /></Col>
             </Row>
         </Container>
     )
