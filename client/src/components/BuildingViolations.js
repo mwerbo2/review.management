8698e72083d4e5bbbc251b1289a245d833fe1bb1
Michael Werbowsky <mwerbo2@gmail.com>
2020-01-27 21:53:46 -0600

    Add in building reviews container

--- client/src/components/BuildingViolations.js
+++ client/src/components/BuildingViolations.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Container from 'react-bootstrap'
+import Row from 'react-bootstrap'
+import Col from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function BuildingViolations() {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    <Row>Date: {props.reportDate}</Row>
+                    <Row>Status: {props.reportStatus}</Row>
+                </Col>
+                <Col>
+                    Description: {props.reportDescription}
+                </Col>
+                <Col>
+                    <FontAwesomeIcon icon={faThumbsUp} />{props.reportThumbsUp}
+                    <FontAwesomeIcon icon={faThumbsDown} />{props.reportThumbsDown}
+                </Col>
+            </Row>
+        </Container>
     )
 }
