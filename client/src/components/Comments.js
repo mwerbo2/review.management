27d75419e22b37e5ac598e0c1cbc8036d179e0dd
Michael Werbowsky <mwerbo2@gmail.com>
2020-01-29 17:58:16 -0600

    Review and rating comment container

---
+++ client/src/components/Comments.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Container from 'react-bootstrap'
+import Row from 'react-bootstrap'
+import Col from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+
+export default function Comments() {
+    return (
+        <Container>
+        <Row>
+            <Col>
+                <Row>Name: {props.comment.name}</Row>
+                <Row>Date: {props.comment.date}</Row>
+            </Col>
+            <Col>
+                {props.comment.body}
+            </Col>
+            <Col>
+                <FontAwesomeIcon icon={faThumbsUp} />{props.comment.reviewThumbsUp}
+                <FontAwesomeIcon icon={faThumbsDown} />{props.comment.reviewThumbsDown}
+            </Col>
+        </Row>
+    </Container>
+    )
+}
