860bb9ca5ba47c183bee8c9fb2c51bb87cc9191a
Michael Werbowsky <mwerbo2@gmail.com>
2020-02-19 22:43:43 -0600

    Login route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
+import Login from './components/Login';
 import SearchResults from './components/SearchResultsContainer';
 import UnitResult from './components/UnitResultContainer';
 
@@ -15,6 +16,7 @@ function App() {
       <Router>
         <Fragment>
           <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
           <Route exact path='/search' component={SearchResults} />
           <Route exact path='/result' component={UnitResult} />
         </Fragment>
